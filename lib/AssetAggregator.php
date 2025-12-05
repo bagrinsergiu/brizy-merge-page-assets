413fdf4fc5864cebe9d557be475f10dc26799262
Zaharia Alexandru <alecszaharia@gmail.com>
2025-12-05 14:09:07 +0200

    refactor: simplify asset processing logic in AssetAggregator and update tests

--- lib/AssetAggregator.php
+++ lib/AssetAggregator.php
@@ -90,8 +90,8 @@ class AssetAggregator
         });
 
         return [
-            isset($freeGroups[0])?$freeGroups[0]->getLibsMap():null,
-            isset($proGroups[0])?$proGroups[0]->getLibsMap():null,
+            isset($freeGroups[0]) ? $freeGroups[0]->getLibsMap() : null,
+            isset($proGroups[0]) ? $proGroups[0]->getLibsMap() : null,
         ];
     }
 
@@ -102,13 +102,7 @@ class AssetAggregator
 
         foreach ($groups as $group) {
 
-            /**
-             * @var AssetGroup $group ;
-             */
-            // set main asset and override if there are pro main assets
-            if (!$mainAsset || $group->getMain()->isPro()) {
-                $mainAsset = $group->getMain();
-            }
+            $assets[] = $group->getMain();
 
             foreach ($group->getGeneric() as $asset) {
                 $assets[] = $asset;
@@ -148,11 +142,6 @@ class AssetAggregator
                     return !is_null($a);
                 }
             );
-        }
-
-        // include main asset
-        if ($mainAsset) {
-            $assets[] = $mainAsset;
         }
 
         return $assets;
@@ -287,8 +276,7 @@ class AssetAggregator
 
                 unset($assets[$i]);
 
-                if(isset($matches[2]))
-                {
+                if (isset($matches[2])) {
                     $matchTermination = $matches[2];
                 }
             }
